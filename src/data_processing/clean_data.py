0a55fffb1c0834f9d1c52269a88e4b557409b9c6
jettjacobus <jettgj2@illinois.edu>
2026-03-07 15:15:57 -0600

    Created clean_data.py data processing file to create an updated csv file with gpa values

---
+++ src/data_processing/clean_data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+df = pd.read_csv('data/raw/uiuc-gpa-dataset.csv')
+df = df.dropna(subset=['Primary Instructor'])
+df["Course"] = df["Subject"] + " " + df["Number"].astype(str)
+df["GPA_Points"] = (
+    (df["A+"] *  4.0) +
+    (df["A"] * 4.0) +
+    (df["A-"] * 3.7) +
+    (df["B+"] * 3.3) +
+    (df["B"] * 3.0) +
+    (df["B-"] * 2.7) +
+    (df["C+"] * 2.3) +
+    (df["C"] * 2.0) +
+    (df["C-"] * 1.7) +
+    (df["D+"] * 1.3) +
+    (df["D"] * 1.0) +
+    (df["D-"] * 0.7) +
+    (df["F"] * 0.0)
+)
+
+df["Average_GPA"] = df["GPA_Points"] / df["Students"]
+
+df.to_csv("data/processed/cleaned_grades.csv", index = False)
