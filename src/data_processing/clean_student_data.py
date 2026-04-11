1e44ac176cf8876e82917dd0aeaaeb38189992fc
jettjacobus <jettgj2@illinois.edu>
2026-04-11 15:51:05 -0500

    added new dataset to predict student strength

---
+++ src/data_processing/clean_student_data.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+
+df = pd.read_csv("data/raw/student_performance_updated_1000.csv")
+
+df = df.dropna()
+
+df = df[df["StudyHoursPerWeek"] >= 0]
+df = df[df["AttendanceRate"] <= 100]
+
+
+
+to_drop = ["Name","Gender","ExtracurricularActivities","ParentalSupport","Study Hours","Attendance (%)","Online Classes Taken"]
+df = df.drop(columns=to_drop)
+
+df.to_csv("data/processed/cleaned_students.csv", index=False)
